4614eabf7b21dbbbc069bd6fdfaa2d8cc7935d32
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-04-14 22:19:51 +0900

    Setting react-query

---
+++ src/hooks/RQProvider.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React, { Suspense, useEffect, useState, lazy } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import("@tanstack/react-query-devtools/build/modern/production.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
+
+function RQProvider({ children }: Props) {
+  const [showDevtools, setShowDevtools] = useState(false);
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        // react-query 전역 설정
+        queries: {
+          refetchOnWindowFocus: false,
+          retryOnMount: true,
+          refetchOnReconnect: false,
+          retry: false,
+        },
+      },
+    })
+  );
+
+  useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools
+        initialIsOpen={process.env.NEXT_PUBLIC_MODE === "local"}
+      />
+      {showDevtools && (
+        <Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </Suspense>
+      )}
+    </QueryClientProvider>
+  );
+}
+
+export default RQProvider;
