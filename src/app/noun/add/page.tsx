438c833ca68f28d733dacceac1ff764a26353e8c
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-04-14 23:41:10 +0900

    Create noun using react-query mutation

--- src/app/noun/add/page.tsx
+++ src/app/noun/add/page.tsx
@@ -2,27 +2,46 @@
 
 import React, { FormEvent, useState } from "react";
 import styles from "@/app/ui/noun/addNoun/addNoun.module.css";
+import { useMutation } from "@tanstack/react-query";
 
 const AddNounPage: React.FC = () => {
   const [content, setContent] = useState("");
-  const handleClick = (event) => {
-    event.preventDefault(); // 이벤트의 기본 동작을 중단
-    console.log(1);
-    console.log("Attempting to add noun with content:", content);
-    mutate(content, {
-      onSuccess: (response) => {
-        console.log("Noun added successfully", response.data);
-        setContent("");
-      },
-      onError: (error) => {
-        console.error("Error adding noun", error);
-      },
-    });
+
+  const postNoun = async (content: string) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}schdic/api/v1/dic/noun`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ content: content }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("잘못된 요청입니다.");
+      }
+      return response.json();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const { mutate } = useMutation({
+    mutationKey: "postNoun",
+    mutationFn: postNoun,
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate(content);
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <input
           type="text"
           placeholder="사용자 사전"
@@ -31,10 +50,10 @@ const AddNounPage: React.FC = () => {
           value={content}
           onChange={(e) => setContent(e.target.value)}
         />
-        <button onClick={handleClick} className={styles.button}>
+        <button type="submit" className={styles.button}>
           등록하기
         </button>
-      </div>
+      </form>
     </div>
   );
 };
