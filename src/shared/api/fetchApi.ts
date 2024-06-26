8031d993ac5367bbab2d8065b8bf43dcea520edc
홍종훈 <whdgnszz1@snu.ac.kr>
2024-06-26 19:58:23 +0900

    React-query 세팅

--- src/shared/api/fetchApi.ts
+++ src/shared/api/fetchApi.ts
@@ -1,36 +1,77 @@
-import { CreateNounDto } from "@/lib/noun";
-import { CreateSynonymDto } from "@/lib/synonym";
+import { CreateSrchUserKywrDto, PutSrchUserKywrDto } from "@/lib/user-keyword";
+import { CreateSrchSynmKywrDto } from "@/lib/synm-keyword";
+import { ApiResponse } from "../types/api-response";
 
 interface FetchAPIOptions {
   method: "GET" | "POST" | "PUT" | "DELETE";
-  body?: CreateNounDto | CreateSynonymDto | FormData | null;
+  body?:
+    | CreateSrchUserKywrDto
+    | CreateSrchSynmKywrDto
+    | PutSrchUserKywrDto
+    | FormData
+    | null;
+}
+
+interface RequestHeaders {
+  "Content-Type"?: string;
+}
+
+function isString(value: any): value is string {
+  return typeof value === "string";
+}
+
+function isAdditionalInfoStringOnly(info: any): boolean {
+  return Object.values(info).every(isString);
 }
 
 export async function fetchAPI<T>(
   url: string,
   options: FetchAPIOptions
-): Promise<T> {
+): Promise<ApiResponse<T>> {
   const { method, body } = options;
 
-  const headers =
-    body instanceof FormData ? {} : { "Content-Type": "application/json" };
+  const headers: RequestHeaders = {
+    "Content-Type": body instanceof FormData ? undefined : "application/json",
+  };
+
+  const filteredHeaders: Record<string, string> = Object.entries(
+    headers
+  ).reduce((acc, [key, value]) => {
+    if (value !== undefined) {
+      acc[key] = value;
+    }
+    return acc;
+  }, {} as Record<string, string>);
 
   const config: RequestInit = {
     method: method,
     credentials: "include",
-    headers: headers,
+    headers: filteredHeaders,
     body: body instanceof FormData ? body : body ? JSON.stringify(body) : null,
   };
 
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/schm/api/v1/dic${url}`,
-      config
-    );
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+    const response = await fetch(`/schm/api/v1/dic${url}`, config);
+
+    if (response.ok) {
+      return (await response.json()) as ApiResponse<T>;
+    } else {
+      const errorData = await response.json();
+      const errorMessage = errorData?.detailMessage || "Unknown error occurred";
+      console.error(`Error with ${method} request: ${errorMessage}`, errorData);
+
+      const error = new Error(errorMessage) as any;
+
+      if (errorData?.additionalInfo) {
+        if (isAdditionalInfoStringOnly(errorData.additionalInfo)) {
+          error.failedSrchUserKywr = errorData.additionalInfo;
+        } else {
+          error.failedSrchSynmKywr = errorData.additionalInfo;
+        }
+      }
+
+      throw error;
     }
-    return (await response.json()) as T;
   } catch (error) {
     console.error(`Error with ${method} request:`, error);
     throw error;
